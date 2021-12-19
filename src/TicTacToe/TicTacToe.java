6f07b7ee886b10279731a51d3f43751fa95aded7
Lavrov529st <m.o.lavrov@student.khai.edu>
2021-12-19 22:51:11 +0200

    TicTacToe 1-st stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X X");
+        System.out.println("O O X");
+    }
+}
